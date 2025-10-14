401d2301d274ab1bdba0a76b58714244a21af56a
ahariani2 <ahariani2@gmail.com>
2025-10-14 17:02:49 -0400

    Update script.js
    Remove lightbox code - add to its own directory

--- script.js
+++ script.js
@@ -46,44 +46,7 @@ listingsData.forEach(listing => {
         const itemListName = 'Physical Product Listings';
         const itemListId = 'listings';
         
-        /** Lightbox Item **/
-        
-        
-const lightbox = document.getElementById('product-lightbox');
-const lightboxDetailsContainer = document.getElementById('lightbox-details-container');
-const closeButton = document.querySelector('.close-button');
-
-
-function openLightbox(listing) {
-  // Populate the lightbox with product details using the new structure
-  lightboxDetailsContainer.innerHTML = `
-    <div class="lightbox-image-container">
-      <img src="${listing.imageUrl}" alt="${listing.shortenedTitle || listing.title}">
-    </div>
-    <div class="lightbox-text-content">
-      <h3>${listing.title}</h3>
-      <p>${listing.subtitle}</p>
-      <div class="lightbox-buttons">
-        ${listing.etsyLink ? `<a href="${listing.etsyLink}" target="_blank" class="lightbox-button etsy-button">View on Etsy</a>` : ''}
-        ${listing.amazonLink ? `<a href="${listing.amazonLink}" target="_blank" class="lightbox-button amazon-button">View on Amazon</a>` : ''}
-        ${listing.michaelsLink ? `<a href="${listing.michaelsLink}" target="_blank" class="lightbox-button michaels-button">View on Michaels</a>` : ''}
-      </div>
-    </div>
-    `;
-
-  lightbox.style.display = 'block'; // Show the lightbox
-}
-
-closeButton.addEventListener('click', () => {
-  lightbox.style.display = 'none'; // Hide the lightbox
-});
-
-// Close the lightbox if the user clicks outside of it
-window.addEventListener('click', (event) => {
-  if (event.target === lightbox) {
-    lightbox.style.display = 'none';
-  }
-});
+   
 
       
   /** Filter Bar **/
