305a279fccab7a7cfd42f0415dd768b61fb9e3a7
ahariani2 <ahariani2@gmail.com>
2025-10-14 17:05:23 -0400

    Update lightbox-script.js
    Format

--- lightbox/lightbox-script.js
+++ lightbox/lightbox-script.js
@@ -1,14 +1,11 @@
-     /** Lightbox Item **/
-        
-        
+/** Lightbox Item **/
 const lightbox = document.getElementById('product-lightbox');
 const lightboxDetailsContainer = document.getElementById('lightbox-details-container');
 const closeButton = document.querySelector('.close-button');
 
-
 function openLightbox(listing) {
-  // Populate the lightbox with product details using the new structure
-  lightboxDetailsContainer.innerHTML = `
+    // Populate the lightbox with product details using the new structure
+    lightboxDetailsContainer.innerHTML = `
     <div class="lightbox-image-container">
       <img src="${listing.imageUrl}" alt="${listing.shortenedTitle || listing.title}">
     </div>
@@ -21,18 +18,17 @@ function openLightbox(listing) {
         ${listing.michaelsLink ? `<a href="${listing.michaelsLink}" target="_blank" class="lightbox-button michaels-button">View on Michaels</a>` : ''}
       </div>
     </div>
-    `;
-
-  lightbox.style.display = 'block'; // Show the lightbox
+  `;
+    lightbox.style.display = 'block'; // Show the lightbox
 }
 
 closeButton.addEventListener('click', () => {
-  lightbox.style.display = 'none'; // Hide the lightbox
+    lightbox.style.display = 'none'; // Hide the lightbox
 });
 
 // Close the lightbox if the user clicks outside of it
 window.addEventListener('click', (event) => {
-  if (event.target === lightbox) {
-    lightbox.style.display = 'none';
-  }
+    if (event.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
 });
